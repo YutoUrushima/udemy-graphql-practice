2e5ad133ae5fa0bb9e07f3f0978d69ee177f3a30
yuto urushima <yuto259233@gmail.com>
2021-05-07 21:51:44 +0900

    nextボタン

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import { Query } from 'react-apollo'
 import client from './client'
 import { SEARCH_REPOSITRIES } from './graphql'
 
+
+const PER_PAGE = 5;
 const DEFAULT_STATE = {
-  first: 5,
+  first: PER_PAGE,
   after: null,
   last: null,
   before: null,
@@ -28,6 +30,14 @@ class App extends Component {
   }
   handleSubmit(event) {
     event.preventDefault();
+  }
+  goNext(search) {
+    this.setState({
+      first: PER_PAGE,
+      after: search.pageInfo.endCursor,
+      last: null,
+      before: null
+    })
   }
   render() {
     const { query, first, last, before, after } = this.state
@@ -60,12 +70,22 @@ class App extends Component {
                         const node = edge.node
                         return (
                           <li key={node.id}>
-                            <a href={node.url} target="_blank">{node.name}</a>
+                            <a href={node.url} target="_blank" rel="noopener noreferrer">{node.name}</a>
                           </li>
                         )
                       })
                     }
                   </ul>
+                  {
+                    search.pageInfo.hasNextPage === true ?
+                      <button
+                        onClick={this.goNext.bind(this, search)}
+                      >
+                        Next
+                      </button>
+                      :
+                      null
+                  }
                 </React.Fragment>
               )
             }
