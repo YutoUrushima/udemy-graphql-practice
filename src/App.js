1931713a5dcd6c4e3b251d682e00d80a5853c396
yuto urushima <yuto259233@gmail.com>
2021-05-07 22:38:31 +0900

    スターを押したときに増えるようにした　mutation使用

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react'
-import { ApolloProvider } from 'react-apollo'
-import { Query } from 'react-apollo'
+import { ApolloProvider, Mutation, Query } from 'react-apollo'
 import client from './client'
-import { SEARCH_REPOSITRIES } from './graphql'
+import { ADD_STAR, SEARCH_REPOSITRIES } from './graphql'
 
 const StarButton = props => {
   const node = props.node
   const totalCount = node.stargazers.totalCount
   const viewerHasStarred = node.viewerHasStarred
   const starCount = totalCount === 1 ? "1 star" : `${totalCount} stars`
-  return (
-    <button>
+  const StarStatus = ({addStar}) => {
+    return (
+      <button
+      onClick={
+        () => addStar({
+            variables: {input: {starrableId: node.id}}
+        })
+      }
+      >
       {starCount} | {viewerHasStarred ? 'starred' : '-'}
-    </button>
+      </button>
+    )
+  }
+  return (
+    <Mutation mutation={ADD_STAR}>
+      {
+        addStar => <StarStatus addStar={addStar}/>
+      }
+    </Mutation>
   )
 }
 
